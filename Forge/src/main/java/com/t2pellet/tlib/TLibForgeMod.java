a4b3f49690d7193fa3f5615409a720a9e215109a
t2pellet <t2pellet@uwaterloo.ca>
2024-06-23 12:40:50 -0400

    fix: server startup issue

--- Forge/src/main/java/com/t2pellet/tlib/TLibForgeMod.java
+++ Forge/src/main/java/com/t2pellet/tlib/TLibForgeMod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -21,6 +22,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public abstract class TLibForgeMod {
 
@@ -50,7 +52,7 @@ public abstract class TLibForgeMod {
         // Common init
         onCommonSetup();
         // Client init
-        onClientSetup();
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> this::onClientSetup);
         // Register into deferred registers
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ENTITIES.register(bus);
@@ -82,7 +84,6 @@ public abstract class TLibForgeMod {
         }
     }
 
-    @OnlyIn(Dist.CLIENT)
     private void onClientSetup() {
         if (clientMod != null) {
             ClientRegistrar.INSTANCE.registerFromClass(modid, clientMod.entityModels());

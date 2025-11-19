fbe69c6be54c2c3f883f0512f2e1b2d48cf916ed
t2pellet <t2pellet@uwaterloo.ca>
2025-11-18 21:23:07 -0500

    feat: version safe registries

---
+++ src/main/java/com/t2pellet/haybale/common/utils/VersionSafeRegistries.java
@@ -0,0 +1,44 @@
+package com.t2pellet.haybale.common.utils;
+
+import net.minecraft.core.DefaultedRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+//? if >= 1.19.3 {
+/*import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+*///?} else {
+//?}
+
+public class VersionSafeRegistries {
+    private VersionSafeRegistries() {}
+
+    public static DefaultedRegistry<Item> item() {
+        //? if >= 1.19.3 {
+        /*return BuiltInRegistries.ITEM;
+        *///?} else
+        return Registry.ITEM;
+    }
+
+    public static ResourceKey<Registry<Item>> itemKey() {
+        //? if >= 1.19.3 {
+        /*return Registries.ITEM;
+        *///?} else
+        return Registry.ITEM_REGISTRY;
+    }
+
+    public static DefaultedRegistry<Block> block() {
+        //? if >= 1.19.3 {
+        /*return BuiltInRegistries.BLOCK;
+        *///?} else
+        return Registry.BLOCK;
+    }
+
+    public static ResourceKey<Registry<Block>> blockKey() {
+        //? if >= 1.19.3 {
+        /*return Registries.BLOCK;
+        *///?} else
+        return Registry.BLOCK_REGISTRY;
+    }
+}
